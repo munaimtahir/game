8a645d6225142f30a293672e957245be089187d2
munaim <munaim@pmc.edu.pk>
2026-05-06 07:18:50 +0000

    fix: import missing performClick in LifecyclePauseTest.kt

--- app/src/androidTest/java/com/vexel/offlinearcade/LifecyclePauseTest.kt
+++ app/src/androidTest/java/com/vexel/offlinearcade/LifecyclePauseTest.kt
@@ -4,6 +4,7 @@ import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.hasText
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.performClick
 import androidx.compose.ui.test.performTouchInput
 import androidx.compose.ui.test.click
 import androidx.lifecycle.Lifecycle
