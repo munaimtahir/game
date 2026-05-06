beae8779638cd5045b03ed3dc899aa8bfcbfae4f
munaim <munaim@pmc.edu.pk>
2026-05-06 02:21:45 +0000

    fix: import missing testTag in game detail screens

--- game/pulseorbit/src/main/java/com/vexel/offlinearcade/game/pulseorbit/PulseOrbitDetailScreen.kt
+++ game/pulseorbit/src/main/java/com/vexel/offlinearcade/game/pulseorbit/PulseOrbitDetailScreen.kt
@@ -9,6 +9,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.vexel.offlinearcade.core.model.GameStats
--- game/stackdrop/src/main/java/com/vexel/offlinearcade/game/stackdrop/StackDropDetailScreen.kt
+++ game/stackdrop/src/main/java/com/vexel/offlinearcade/game/stackdrop/StackDropDetailScreen.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.unit.dp
 import com.vexel.offlinearcade.core.model.GameStats
 import com.vexel.offlinearcade.core.ui.ArcadeCard
